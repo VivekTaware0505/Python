dce0cb0d2e4f2953ca757c64bea8eeb840e2ef8e
VIVEK TAWARE <vivektaware16@gmail.com>
2026-02-19 20:25:36 +0530

    some changes

--- day_five_python.py
+++ day_five_python.py
@@ -6,5 +6,5 @@ str2 = """ hallo vivek how are you and hows going your python learning """
 
 num1 = int ("11")
 num2 = float (1.11)
-print(num1+num2,"this type casting of integer value ")
-print(num1 + num2)
+
+print(num1+num2," value after addition  ")
