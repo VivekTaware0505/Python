b15de088081e10aae424e458a22a06f7f28c92f7
VIVEK TAWARE <vivektaware16@gmail.com>
2026-02-19 20:15:18 +0530

    string examles and type casting exmaples done

---
+++ day_five_python.py
@@ -0,0 +1,10 @@
+str ='vivek'
+str1 = "hallo vivek ? "
+str2 = """ hallo vivek how are you and hows going your python learning """
+
+# type casting examples 
+
+num1 = int ("11")
+num2 = float (1.11)
+print(num1+num2,"this type casting of integer value ")
+print(num1 + num2)
