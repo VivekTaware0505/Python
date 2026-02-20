aa7466d6919e20ee39d61eb012c460dbee18fd4c
VIVEK TAWARE <vivektaware16@gmail.com>
2026-02-20 22:07:05 +0530

    index number \ printing characters using ndex number as we want

--- day_five_python.py
+++ day_five_python.py
@@ -8,3 +8,13 @@ num1 = int ("11")
 num2 = float (1.11)
 
 print(num1+num2," value after addition  ")
+
+
+# index number \ printing characters using ndex number as we want 
+
+name = "vivek_taware"
+
+print(name[0:4])
+print(name[2:])
+print(name[:6])
+print(name[1:9])
