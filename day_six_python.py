66d1ca94d65833d99f34473b013000b484679c60
VIVEK TAWARE <vivektaware16@gmail.com>
2026-02-23 22:05:25 +0530

    ends with function and replace function

--- day_six_python.py
+++ day_six_python.py
@@ -6,3 +6,14 @@ print(str[1:6])
 print(str[13])
 print(str[:12])
 print(str[5:])
+
+#ends with function
+
+str = "apna college india from pune india"
+str = str.capitalize()
+print(str.endswith("ia"))
+print(str.capitalize())
+
+# replace
+print(str.replace("o","V"))
+print(str.replace("pune","akluj"))
