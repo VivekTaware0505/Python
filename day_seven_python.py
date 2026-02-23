80aee2ce0c65705b138d16ee85a14180bf0e0491
VIVEK TAWARE <vivektaware16@gmail.com>
2026-02-23 22:10:50 +0530

    practice questions / wrtie a program to input user first and print its len

--- day_seven_python.py
+++ day_seven_python.py
@@ -1,5 +1,20 @@
 #count function that returns which word we want to he return that word how many time present in our string
 
+
+str = "apna college india from pune india"
 print(str.count("f"))
 print(str.count("p"))
 print(str.count("india"))
+
+
+
+#practice questions 
+#write a program to input users first name and print its length
+
+
+name = input("enter your name : ")
+surname = input("enter your suname : ")
+print(surname)
+print("length of your surname ",len(surname))
+print(name)
+print("length of your name ",len(name))
