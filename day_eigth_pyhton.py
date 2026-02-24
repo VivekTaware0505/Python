b6f60e188d4e14d010e7b56518f6ed50eec47cb8
VIVEK TAWARE <vivektaware16@gmail.com>
2026-02-24 15:11:01 +0530

    if elif and else conditional statement traffic light examples

--- day_eigth_pyhton.py
+++ day_eigth_pyhton.py
@@ -1,8 +1,21 @@
-# if elif else 
+# condotional statements if elif else 
 
-age = int(input("enter your age"))
+age = int(input("enter your age : "))
 
 if(age>=18):
     print("you can vote")
 else:
     print("you are not eligible for vote")
+
+    # traffic light example for if and elif
+
+light = input("enter light colour : ")
+
+if(light == "green"):
+    print("safe you can go now")
+elif(light == "red"):
+    print("wait for green light")
+elif(light == "yellow"):
+    print("look and go slow")
+else:
+    print("light is broken")
