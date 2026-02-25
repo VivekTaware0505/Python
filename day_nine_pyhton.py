c4834e890fdd3fd8d9943a78c27162fada862b74
VIVEK TAWARE <vivektaware16@gmail.com>
2026-02-25 21:32:14 +0530

    write a program to find the greatest of 3 numbers entered bye the user

--- day_nine_pyhton.py
+++ day_nine_pyhton.py
@@ -5,6 +5,20 @@ num = int(input("enter number to check no is odd or even : "))
 
 
 if(num % 2 == 0 ):
-    print("number is odd ")
+    print("number is EVEN ")
 else:
-    print("number is even ")
+    print("number is ODD ")
+
+
+# write a program to find the greatest of 3 numbers entered bye the user
+
+a = int(input("enter first number : "))
+b = int(input("enter second number : "))
+c = int(input("enter third number : "))
+
+if(a >= b and b >= c):
+    print("first number is largest : " , a )
+elif(b >= c):
+    print("second number is largest : " , b)
+else:
+    print("third number is largest : ", c)
