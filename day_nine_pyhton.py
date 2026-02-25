b612d2fccdfc9f9366393fe762fcd2614cc57693
VIVEK TAWARE <vivektaware16@gmail.com>
2026-02-25 21:14:42 +0530

    write a program to check the entered by the user is odd or even

---
+++ day_nine_pyhton.py
@@ -0,0 +1,10 @@
+#write a program to check the entered by the user is odd or even
+
+num = int(input("enter number to check no is odd or even : "))
+
+
+
+if(num % 2 == 0 ):
+    print("number is odd ")
+else:
+    print("number is even ")
