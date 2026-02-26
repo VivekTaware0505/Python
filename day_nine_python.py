85eaa53a3782a5a5c26b857d6b847fc6e023888a
VIVEK TAWARE <vivektaware16@gmail.com>
2026-02-26 22:20:39 +0530

    lists are chanble it means we can change values of lists

--- day_nine_python.py
+++ day_nine_python.py
@@ -1,13 +1,17 @@
 # tuples and list lecture No : 03
+# lists are mutable means changeble 
 
-student_data = ["vivek", 60 ,"malinagar"]
+student_data = ["vivek", 60,55 ,"malinagar"]
 
-print(student_data)
+print(student_data[0])
 print(len(student_data))
 print(type(student_data))
+student_data [0] = "akshay"
+print(student_data)
 
 
 marks = [ 99.5,98.2,65.2,78.2,98.5]
 print(marks)
+print(marks[0])
 
 
