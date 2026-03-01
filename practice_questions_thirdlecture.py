9cba17d3d02da3a16f5a57ba6e30981982e22bf6
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-01 13:40:06 +0530

    program to count the number of students with A grade in following tuple

--- practice_questions_thirdlecture.py
+++ practice_questions_thirdlecture.py
@@ -11,3 +11,11 @@ M_list.insert(0,movie_name1)
 M_list.insert(1,movie_name2)
 M_list.insert(2,movie_name3)
 print(M_list)
+
+
+# write a program to to count the number of students with the A grade in the following tuple
+
+grade_tup =("C","D","A","A","B","B","A")  
+
+grade_tup.count("A")
+print(grade_tup)
