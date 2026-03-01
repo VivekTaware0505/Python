5ab529345dc845aec09a38c7a5ae9378583ebb92
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-01 22:30:32 +0530

    movie input program  with three different types of program methods

--- practice_questions_thirdlecture.py
+++ practice_questions_thirdlecture.py
@@ -6,11 +6,32 @@ movie_name1 = input("enter movie names : ")
 movie_name2 = input("enter movie names : ")
 movie_name3 = input("enter movie names : ")
 
-M_list = [""]
-M_list.insert(0,movie_name1)
-M_list.insert(1,movie_name2)
-M_list.insert(2,movie_name3)
+
+M_list2 = []
+M_list2.append(input("enter movie names : "))
+M_list2.append(input("enter movie names : "))
+M_list2.append(input("enter movie names : "))
+print(M_list2)
+print("-------------------------------------- vivek learning pyhton -----------------------------------------------------------")
+
+
+M_list = []
+M_list1 = []
+
+
+
+M_list.append(movie_name1)
+M_list.append(movie_name2)
+M_list.append(movie_name3)
 print(M_list)
+ 
+print("-------------------------------------- vivek learning pyhton -----------------------------------------------------------")
+
+
+M_list1.insert(0,movie_name1)
+M_list1.insert(1,movie_name2)
+M_list1.insert(2,movie_name3)
+print(M_list1)
 
 
 print("-------------------------------------- vivek learning pyhton -----------------------------------------------------------")
@@ -26,4 +47,5 @@ print(grade_tup.count("A"))
 print("-------------------------------------- vivek learning pyhton -----------------------------------------------------------")
 
 
-#
+# write a peogram to check if a list contains a palindrome of element (hint : use cop() method)
+# [1,2,3,2,1]   [1, "abc" , "abc" , 1]
