cfac9f2b3cdd22f892d22c734bd3f9f20b0dbcb8
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-03 19:02:21 +0530

    dictonaries in pyhton '

---
+++ day_11_python.py
@@ -0,0 +1,10 @@
+# dictionary in python 
+
+print("-------------------------------------- vivek learning pyhton -----------------------------------------------------------")
+
+info = {
+    "name" :"vivek",
+    "subjects" : "java",
+    "marks" : 76
+}
+print(info)
