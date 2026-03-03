bfeeb7efb656621025ff6660b8ad33695e5427e2
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-03 22:56:59 +0530

    used in push dictionary

--- day_11_python.py
+++ day_11_python.py
@@ -20,4 +20,8 @@ info_new = {
     "marks": (60,76)
 }
 info_new["topics"]="learnig","analysis"
+info_new["locatin"] ="malinagar",5,"ganeshgao",15
+info_new["vehicle name"] = input("enter your vehicle name : ")
 print(info_new)
+
+
