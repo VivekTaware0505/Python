77322c78edf10e8391c708492d1d0831c4df092e
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-04 19:45:24 +0530

    new method

--- day_11_python.py
+++ day_11_python.py
@@ -19,7 +19,7 @@ info_new = {
     "names": ["vivek","akshay"],
     "marks": (60,76)
 }
-info_new["topics"]="learnig","analysis"
+info_new["topics"]="learnig","analysis" # this means overrite the that point 
 info_new["locatin"] ="malinagar",5,"ganeshgao",15
 info_new["vehicle name"] = input("enter your vehicle name : ")
 print(info_new)
