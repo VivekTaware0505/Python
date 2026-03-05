fbe1d76be9e8f0b320eaaa50ee19b98b9c3ecf4f
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-05 23:04:32 +0530

    accessing values using methods

--- day_12_python.py
+++ day_12_python.py
@@ -24,3 +24,14 @@ print("-------------------------------------- vivek learning pyhton ------------
 
 print(info.values()) #values method it returns all value pairs into tuples
 print(info.get("name")) # returns the key according to value
+
+print("-------------------------------------- vivek learning pyhton -----------------------------------------------------------")
+
+subject = {
+
+    "math":99,
+    "java":99,
+    "pyhton":99
+}
+
+print(subject.get("pyhton"))
