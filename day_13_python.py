fe2e94d3603ea295caabce6dee174073d88e6f72
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-07 14:33:00 +0530

    set in pyhton set is collection of unordered items

--- day_13_python.py
+++ day_13_python.py
@@ -1,2 +1,16 @@
 # set in python 
+        # set is the collection of unorderd items 
 
+collection ={1,2,3,4}
+
+print(collection)
+print(type(collection))
+
+
+print("-------------------------------------- vivek learning pyhton -----------------------------------------------------------")
+
+
+collection1 = {"hallo", 1 , 2,2,2,"world","world"}
+
+print(collection1)  # in the set he ignores the duplicate items and prints only once
+print(len(collection1))
