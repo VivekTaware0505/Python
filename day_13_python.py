33cdc1ed5be30c281484b81d41c29f867d25221e
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-07 14:43:54 +0530

    methods in set

--- day_13_python.py
+++ day_13_python.py
@@ -14,3 +14,20 @@ collection1 = {"hallo", 1 , 2,2,2,"world","world"}
 
 print(collection1)  # in the set he ignores the duplicate items and prints only once
 print(len(collection1))
+
+
+
+print("-------------------------------------- vivek learning pyhton -----------------------------------------------------------")
+
+
+
+collection3 = set()
+
+collection3.add("vivek")
+collection3.add(90)
+collection3.add(556)
+
+print(collection3)
+
+collection3.remove(556)
+print(collection3)
