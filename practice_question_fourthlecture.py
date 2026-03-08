d798b0a8a37cd43aea79da4430a0a75e51105bd4
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-08 10:22:29 +0530

    Q .1 store following word meaning in python dictionary

---
+++ practice_question_fourthlecture.py
@@ -0,0 +1,12 @@
+# Q .1 store following word meaning in python dictionary
+# table : "a piece of furniture ","list of facts and figures "
+# cat : "a small animal"
+
+
+table ={
+    "table" : ["a piece of furiniture " , "list of facts and figures"],
+    "cat":"a small animal"
+}
+
+print(table)
+
