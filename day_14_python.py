18675d5cbd62b8b8d9aa1362b3e94c235c0cd566
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-09 14:36:15 +0530

    print serial no wise hallo word for 10 times '

--- day_14_python.py
+++ day_14_python.py
@@ -2,7 +2,16 @@
 word = 1
 
 while word <= 10:
-    print("vivek")
+    print("vivek",word)
     word += 1
 
 print(word)
+
+
+# print serial no wise hallo word for 10 times
+
+i = 1
+
+while i <= 10:
+    print(i,"hallo")
+    i+=1
