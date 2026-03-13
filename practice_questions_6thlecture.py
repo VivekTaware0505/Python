7d13cc4d25157b5f5ebb3055e7ede170b1ff7741
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-13 22:11:34 +0530

    search for a number x in this tuple using loop

--- practice_questions_6thlecture.py
+++ practice_questions_6thlecture.py
@@ -1,9 +1,25 @@
   # print the elements of the following list using a loop
 
-list =[1,4,9,16,25,36,49,64,81,100]
+# list =[1,4,9,16,25,36,49,64,81,100]
  
 
-for element in list:
-    print(element)
-else:
-    print("END")
+# for element in list:
+#     print(element)
+# else:
+#     print("END")
+
+
+# search for a number x in this tuple using loop
+
+
+tup = (1,4,9,16,25,36,49,64,81,100,81,36)
+
+x = 36
+idx = 0
+
+for num in tup:
+    if(num == x):
+        print("we found x ->:", num ,":",idx )
+    idx += 1
+      
+    
