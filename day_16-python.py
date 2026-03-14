d1cc46cb527a21b17ccacb6cc3f6e12aa4c5a5ce
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-14 22:16:58 +0530

    start stop step pionts in range

--- day_16-python.py
+++ day_16-python.py
@@ -6,3 +6,30 @@ print(seq[0])
 print(seq[1])
 print(seq[2])
 print(seq[3])
+
+
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+
+
+for i in seq:
+    print(i)
+
+
+    print("-------------------------------------- vivek learning python only (start)  -----------------------------------------------------------")
+
+
+for a in range(10): # only starting
+    print(a)
+
+print("-------------------------------------- vivek learning python (start,stop) -----------------------------------------------------------")
+
+for b in range(2,10): # (start , stop ) point has marked
+    print(b)
+
+
+print("-------------------------------------- vivek learning python  (start , stop . step )-----------------------------------------------------------")
+
+
+for c in range(3,10,1):  # (start, stop , step) starting and stop and step is pointed step is pointed for increasing count 
+    print(c)
+
