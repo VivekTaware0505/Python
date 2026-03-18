3c65b432c5dc909fc99c4b85f646774792705698
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-18 22:27:14 +0530

    write a function to print the length of a list

---
+++ practice_question_7thlecture.py
@@ -0,0 +1,15 @@
+
+# write a function to print the length of a list 
+
+list1 = [1,2,3,6,65,3,35]
+hallo = ["hallo ","vivek","vivek"]
+
+def li (list):
+   print(len(list))
+   return
+
+li(list1)
+li(hallo)
+
+
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
