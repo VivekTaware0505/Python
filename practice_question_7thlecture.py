0ff0d4f26cbdf1e6605107f442f46058375823dc
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-19 21:24:38 +0530

    write a function to the factorial of n

--- practice_question_7thlecture.py
+++ practice_question_7thlecture.py
@@ -29,3 +29,23 @@ singal(hallo)
 singal(list1)
 
 
+
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+
+
+# write a function to the factorial of n (n is the parameter)
+
+def cal_fact (n):
+   fact = 1
+   for i in range(1 , n+1):
+      fact *= i
+      print(fact)
+
+cal_fact(6)
+
+
+
+
