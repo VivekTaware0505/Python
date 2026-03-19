d0f841fc0286a8d99a0132bb2a8a8e4937356670
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-19 21:36:50 +0530

    write a function to convert USD to INR

--- practice_question_7thlecture.py
+++ practice_question_7thlecture.py
@@ -48,4 +48,22 @@ cal_fact(6)
 
 
 
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+
+
+
+
+
+# write a function to convert USD to INR
+
+
+def  converter ( usd_val):
+   
+  
+   inr_val = usd_val * 83
+
+   print(usd_val,"USD : ",inr_val," : INR")
+
+converter(110)
 
