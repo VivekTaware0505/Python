7f0db4bc7b2aa3a8b5a58aa221cfbcee4ef0bdb4
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-20 23:23:33 +0530

    factorial number using recursion

--- day_18_python.py
+++ day_18_python.py
@@ -5,5 +5,20 @@ def show (n):
         return
     print(n)
     show(n-1)
+    print("END")
 
 show(5)
+
+
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+
+
+# factorial number example in recursion 
+
+
+def cal_fact(v):
+    if(v == 1 or v ==0):
+        return 1
+    return cal_fact(v-1) * v
+
+print(cal_fact(6))
