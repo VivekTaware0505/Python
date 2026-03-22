1cae8917d6d3565652fa5dd0235b1330d3a149f8
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-22 21:24:56 +0530

    check palindrome using recusive function

--- some_questions_of_recursion.py
+++ some_questions_of_recursion.py
@@ -11,4 +11,21 @@ print(revers_string("hallo"))
 print(revers_string("keviv"))
 
 
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
 
+
+# check palindrome using recusive function
+
+def is_palindrom (v):
+
+    if(len(v) <= 1):
+        return True
+    if( v[0] !=  v[-1]):
+        return False
+    
+    return is_palindrom(v[1:-1])
+
+
+print(is_palindrom("madam"))
+print(is_palindrom("Taware "))
