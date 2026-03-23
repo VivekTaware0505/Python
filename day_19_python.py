4e14892e468851ec5017c83d6abbc9d088f850a2
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-23 22:28:07 +0530

    reading methods in file file input output

--- day_19_python.py
+++ day_19_python.py
@@ -6,3 +6,15 @@ data = f.read()
 print(data)
 print(type(data))
 f.close()
+
+
+
+
+# reading methods in file input output 
+
+
+v = open("demo.txt","r")
+datav = v.read(10)
+print(datav)
+print(type(datav))
+v.close()
