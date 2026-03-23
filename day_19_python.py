030acf69c75d4de116d3f3b0b116ac2075cade71
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-23 22:30:25 +0530

    readline method in file I/O

--- day_19_python.py
+++ day_19_python.py
@@ -8,6 +8,7 @@ print(type(data))
 f.close()
 
 
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
 
 
 # reading methods in file input output 
@@ -18,3 +19,18 @@ datav = v.read(10)
 print(datav)
 print(type(datav))
 v.close()
+
+
+
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+
+# readline method in file I/O
+
+
+f= open("demo.txt","r")
+
+datac = f.readline()
+
+print(datac)
+print(type(datac))
+f.close()
