97c83c5c5b386d94be3fdd9c837b6cf325eae932
VIVEK TAWARE <vivektaware16@gmail.com>
2026-03-23 22:40:53 +0530

    writing method

--- day_19_python.py
+++ day_19_python.py
@@ -29,8 +29,27 @@ print("-------------------------------------- vivek learning python  -----------
 
 f= open("demo.txt","r")
 
-datac = f.readline()
+line1 = f.readline()
+line2 = f.readline()
 
-print(datac)
-print(type(datac))
+print(line1)
+print(line2)
+print(type(line1))
 f.close()
+
+
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+
+
+# writing methods in file I/O
+
+w = open("demo.txt","w")
+write = w.write("we are changing the data of existing file ")
+
+print(write)
+print(type(write))
+w.close()
+
+
+
