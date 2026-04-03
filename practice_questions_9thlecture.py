136edc66bdf7e7e78602f4a097a98130f4e9cad6
VIVEK TAWARE <vivektaware16@gmail.com>
2026-04-03 22:50:14 +0530

    create student class that makes name and marks of 3 subject as argument in constructor

---
+++ practice_questions_9thlecture.py
@@ -0,0 +1,20 @@
+# create student class that makes name and marks of 3 subject as argument in constructor 
+# then create a method to print the avrage
+
+
+
+class student:
+
+    def __init__(self, marks, name):
+        self.marks = marks
+        self.name = name
+
+    def get_avg(self):
+        total = 0
+        for val in self.marks:
+            total += val
+        print("Hello", self.name, "your avg is:", total / len(self.marks))
+
+
+s1 = student([97, 98, 99], "vivek")   # ✔ correct order
+s1.get_avg()
