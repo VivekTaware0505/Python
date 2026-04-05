4b22818c775561e5d2abcbf72a3c3f1768b0b5f8
VIVEK TAWARE <vivektaware16@gmail.com>
2026-04-05 15:54:07 +0530

    static method

---
+++ day_22_python.py
@@ -0,0 +1,13 @@
+# static method 
+
+# example
+@staticmethod
+def new_static():
+
+    print("hallo vivek this is static mehtod ")
+
+
+new_static()
+
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
