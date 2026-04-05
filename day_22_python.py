6cf80fdc6287b5eeef5f5982008f103d9bc3d54d
VIVEK TAWARE <vivektaware16@gmail.com>
2026-04-05 20:41:05 +0530

    car start example for ABSTRACTION in python

--- day_22_python.py
+++ day_22_python.py
@@ -11,3 +11,21 @@ new_static()
 
 print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
 print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+
+# car start example for ABSTRACTION in python
+
+
+class car:
+    def __init__(self):
+        self.acc = False
+        self.brk = False
+        self.clutch = False
+
+    def start(self):
+        self.clutch = True
+        self.acc = True
+        print("car started... ")
+
+    
+car1 = car()
+car1.start()
