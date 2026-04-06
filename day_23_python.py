aa031a874710e6528152c3f60896d96c83a25a1e
VIVEK TAWARE <vivektaware16@gmail.com>
2026-04-06 22:27:43 +0530

    example of del keyword in python

--- day_23_python.py
+++ day_23_python.py
@@ -1,2 +1,18 @@
 # del keyword used to delete object properties and delete object itself 
 
+class Student:
+
+    def __init__(self, name):
+        self.name = name
+
+s1 = Student("Vivek")
+print(s1.name)
+
+
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+
+del s1
+print(s1)
+
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
