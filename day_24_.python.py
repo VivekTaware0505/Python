e7c343a17aa13358c79b10f68655f3dfeab5f7cf
VIVEK TAWARE <vivektaware16@gmail.com>
2026-04-10 16:34:35 +0530

    in the inheritance i used private method '

--- day_24_.python.py
+++ day_24_.python.py
@@ -1,1 +1,33 @@
 # Inheritance 
+# when one class ( child or derived ) derives the properties and methods of another class (parent / base )
+
+class car:
+
+    @staticmethod
+    def start():
+        print("car started ... ")
+
+    @staticmethod
+    def stop():
+        print(" car stoped ")
+
+    def __break(self):
+        print("break fail ... ")
+        
+    def failbreak(self):
+        self.__break()
+    
+
+class toyota(car):
+    def __init__(self, name):
+        self.name = name
+    
+    
+
+
+
+car1 = toyota("fortuner")
+car2 = toyota("innova ")
+
+print(car1.start())
+print(car1.failbreak())
