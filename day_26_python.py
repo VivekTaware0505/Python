8d71b006a76eae96c3fd2c3af2dfac2e9ea37025
VIVEK TAWARE <vivektaware16@gmail.com>
2026-04-16 21:23:43 +0530

    example of @property method in python

--- day_26_python.py
+++ day_26_python.py
@@ -1,3 +1,19 @@
 # property 
 # we use @porpety decorator on any method in the class the method as a property 
 
+class student:
+    def __init__(self, phy, chem, math ):
+        self.phy = phy
+        self.chem = chem
+        self.math = math
+
+    @property
+    def percentage(self):
+        return str((self.phy + self.chem + self.math) / 3) +" % "
+
+stu1 = student( 98, 59, 94)
+
+print(stu1.percentage)     
+
+stu1.chem = 91
+print(stu1.percentage)
