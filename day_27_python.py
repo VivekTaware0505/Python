3fc36573e9ff8b0cdf3be3cadee47e9973b3c61d
VIVEK TAWARE <vivektaware16@gmail.com>
2026-04-16 21:36:44 +0530

    example of polymorphism

---
+++ day_27_python.py
@@ -0,0 +1,20 @@
+# Polymorphism :- Operator Overloading 
+# when the same operator is allowed to have different meaning to the context 
+
+# example of polymorphism 
+
+print(1+2) # its trying to additon 
+print("vivek" + "taware") # its trying to concatenet 
+print([1, 2, 3] + [4, 5, 6])  # its trying to merge
+
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+print("-------------------------------------- vivek learning python  -----------------------------------------------------------")
+
+
+# opeators and Dunder Functions 
+
+# a+b -> addition :-  a__add__(b)
+# a-b -> substraction :- a__sub__(b)
+# a*b -> multiplication :- a__mul___(b)
+# a/b -> divide   :- a__truediv____(b)
+# a%b -> addition  :- a__mod____(b)
