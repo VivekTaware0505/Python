6043a92e6110463f26228cb122fb982ec1ccbe03
VIVEK TAWARE <vivektaware16@gmail.com>
2026-04-18 22:31:37 +0530

    practice question no 1

---
+++ practice_questions_lastlecture.py
@@ -0,0 +1,5 @@
+# que no 1 :  define a circle class to create a circle with radius r using the constructor define an Area()
+# method of the class which calculates the area of the circle defone a perimeter() method of the class 
+# which allows you to calculate the perimeter of the circle 
+
+
